444f863f5e4c55623ac6133dca0f4db77ded4ec9
Allan <allanaugusto2077@gmail.com>
2026-06-28 15:10:11 -0300

    Configura dashboard

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estoque/', include('estoque.urls')),
+    path('', include('dashboard.urls')),
 ]
 
 if settings.DEBUG:
